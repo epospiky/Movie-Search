6d6d6a78e574856230092f7835f9990e286b81ce
Ernest <epospiky@gmail.com>
2023-11-15 20:35:48 +0000

    basic framework

---
+++ app/src/App.tsx
@@ -0,0 +1,27 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = [
+    "New York",
+    "San Francisco",
+    "Tokyo",
+    "London",
+    "Lagos",
+    "Paris",
+  ];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/Message.tsx
@@ -0,0 +1,8 @@
+//PascalCasing
+function Message() {
+    //Jsx
+    const name = 'Ernest'
+    return <h1>Hello, world. I am {name}</h1>
+}
+
+export default Message;
