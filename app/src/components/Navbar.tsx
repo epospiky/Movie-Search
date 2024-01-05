a143d9b3f336dc03311bad4a2e0f920a37bb9c08
Ernest <epospiky@gmail.com>
2024-01-05 23:51:15 +0000

    modified navbar for darkmode

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,12 +1,23 @@
+import React, { useState } from "react";
 import logo from "../Images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Search from "./Search";
 
 const Navbar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="row">
-      <nav className="col-12 navbar navbar-expand-lg bg-body-tertiary">
+    <div className={`row ${darkMode ? "darkMode" : ""}`}>
+      <nav
+        className={`col-12 navbar navbar-expand-lg bg-body-tertiary ${
+          darkMode ? "darkMode" : ""
+        }`}
+      >
         <div className="container-fluid">
           <a className="navbar-brand w-25" href="#">
             <img src={logo} alt="Logo" className="w-50 h-50" />
@@ -22,10 +33,35 @@ const Navbar = () => {
             </label>
           </div>
           <div className="mode-box w-25">
-            <div className="mode-box-container">
-              <div className="switch"></div>
-              <span>DarkMode</span>
-            </div>
+            <li
+              className="list-none mode__box "
+              style={{ listStyleType: "none" }}
+            >
+              <div
+                className="mode__box--container rounded-pill"
+                style={{
+                  cursor: "pointer",
+                  background: "gray",
+                  position: "relative",
+                  width: "50px",
+                  height: "25px",
+                  paddingTop: "1px",
+                  paddingLeft: "1px",
+                  paddingRight: "1px",
+                }}
+                onClick={handleModeToggle}
+              >
+                <div
+                  className="switch rounded-circle"
+                  style={{
+                    position: "absolute",
+                    cursor: "pointer",
+                    width: "23px",
+                    height: "23px",
+                  }}
+                ></div>
+              </div>
+            </li>
           </div>
         </div>
       </nav>
