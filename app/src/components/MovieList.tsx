199717df73ba4650e258093643ea53ecb3bf4a1d
Ernest <epospiky@gmail.com>
2024-01-06 23:47:24 +0000

    added rating and release date

--- app/src/components/MovieList.tsx
+++ app/src/components/MovieList.tsx
@@ -5,6 +5,8 @@ interface Movie {
   title: string;
   poster_path: string | null;
   genres: string[];
+  vote_average: number;
+  release_date: string;
 }
 interface MovieListProps {
   movies: Movie[];
@@ -16,7 +18,7 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
       <h1 className="text-center ">Popular Movies</h1>
       <ul className="row" style={{ listStyleType: "none" }}>
         {movies.map((movie) => (
-          <li className="col-md-2" key={movie.id}>
+          <li className="col-md-2 mb-3 mx-2" key={movie.id}>
             {movie.poster_path && (
               <img
                 className="rounded"
@@ -24,9 +26,16 @@ const MovieList: React.FC<MovieListProps> = ({ movies }) => {
                 alt={`${movie.title} Poster`}
               />
             )}
-            <p className="text-uppercase text-wrap text-break fw-semibold fs-6">
+            <p style={{ color: "gray" }} className="fst-italic">
+              {movie.vote_average.toFixed(1)}/10
+            </p>
+            <p
+              style={{ color: "#067488", margin: 0 }}
+              className=" text-wrap text-break fw-semibold h6"
+            >
               {movie.title}
             </p>
+            <p className="fst-normal fw-medium">{movie.release_date}</p>
           </li>
         ))}
       </ul>
